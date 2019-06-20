44c5de95ac0d3e866128d800913891c9fb1c1d16
Kieran <k33k00@pm.me>
2019-06-20 13:36:13 +0100

    Added timers and updated vuex

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue';
 import Buefy from 'buefy';
 import axios from 'axios';
+import VueTimers from 'vue-timers';
 
 import App from './App.vue';
 import router from './router';
+import store from './store'
 import './assets/scss/app.scss';
 
 
+Vue.use(VueTimers);
 Vue.use(Buefy);
 Vue.prototype.$axios = axios;
 
@@ -14,5 +17,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app');
