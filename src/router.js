3892261d269b087f8b5705f2b8917f06a42bf1f2
Kieran <k33k00@pm.me>
2019-06-24 21:20:19 +0100

    Adds new pages
    - Home screen to allow multiple pages and future dash.
    - Recent scanned files page
    - Wired everything up in the router

--- src/router.js
+++ src/router.js
@@ -1,17 +1,28 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Queue from './views/Queue.vue';
+import Home from './views/Home.vue';
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
+  linkExactActiveClass: 'is-active',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'queue',
-      component: Queue,
+      name: 'home',
+      component: Home,
     },
+    {
+      path: '/queue',
+      name: 'queue',
+      component: () => import(/* webpackChunkName: "queue" */ './views/Queue.vue')
+    },
+    {
+      path: '/recent',
+      name: 'recent',
+      component: () => import(/* webpackChunkName: "queue" */ './views/Recent.vue')
+    }
   ],
 });
---
+++ src/views/Recent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <R-Table></R-Table>
+  </div>
+</template>
+
+<script>
+import RTable from '@/components/RTable.vue'
+export default {
+  components: {
+    RTable
+  }
+}
+</script>
+
+<style>
+
+</style>
